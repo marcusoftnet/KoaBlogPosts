e24ef744c8ebc7ae4f4731d4cabad08a72d11771
Marcus Hammarberg <marcusoft.net@gmail.com>
2014-04-05 21:45:59 +0700

    Added CRUD API example

--- KoaFirstStumblingSteps/firstKoaSite.js
+++ KoaFirstStumblingSteps/firstKoaSite.js
@@ -4,16 +4,17 @@ var route = require('koa-route');
 // Create the app
 app = koa();
 
-// I'll use some very simple routes
+// we'll use some very simple routes
 app.use(route.get('/whosThere', noName));
 app.use(route.get('/whosThere/:name', withName));
 
-function *noName() {
+// Here's a couple of functions that returns some nice responses
+function* noName() {
   var res = 'Sadly I cannot properly greet you, Anonymous';
   this.body = res;
 };
 
-function *withName(name) {
+function* withName(name) {
   name = decodeURI(name);
   var res = 'Ah, it is you! My old friend!\nHello ' + name;
   this.body = res;
@@ -22,3 +23,4 @@ function *withName(name) {
 // Let's fire this baby up
 app.listen(3000);
 console.log("This Koa site is now running at http://localhost:3000");
+console.log("Hit it with /whosThere or /whosThere/yourName to get reponses");
